51b9fa83329b8e4eb03db7882061dcf4e8693771
darkyzhou <darkyoooooo@gmail.com>
2021-03-07 17:55:45 +0800

    chore(service-worker): disable service-worker in development mode

--- rollup.config.js
+++ rollup.config.js
@@ -120,7 +120,7 @@ export default {
     onwarn
   },
 
-  serviceworker: {
+  serviceworker: !dev && {
     input: config.serviceworker.input(),
     output: config.serviceworker.output(),
     plugins: [
