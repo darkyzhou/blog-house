a9a689a076d26adc9f8104f77af2fd461115aaa1
darkyzhou <darkyoooooo@gmail.com>
2021-03-08 22:17:19 +0800

    feat(sitemap.xml.js): add support for tags and index page

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,11 +1,12 @@
-const { SitemapStream, streamToPromise } = require('sitemap');
-const { Readable } = require('stream');
-import allPosts from './_posts';
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
+import posts from './_posts';
+import tags from './tags/_tags';
 import constraints from '../../config/constraints.json';
 
 function concatPageUrl(pathName) {
   const url = new URL(constraints.sitemap.urlPrefix);
-  url.pathname = `${pathName}/index.html`;
+  url.pathname = `${pathName}`;
   return url.href;
 }
 
@@ -13,13 +14,31 @@ export async function get(request, response) {
   response.setHeader('Cache-Control', 'max-age=0, s-max-age=600');
   response.setHeader('Content-Type', 'application/xml');
 
+  const now = new Date().toISOString();
   const links = [];
-  for (const post of allPosts) {
-    links.push({
+
+  links.push({
+    url: constraints.sitemap.urlPrefix,
+    lastmod: now,
+    changefreq: 'daily',
+    priority: 0.6
+  });
+
+  links.push(
+    posts.map((post) => ({
       url: concatPageUrl(post.slug),
       lastmod: new Date(post.lastModifiedAt).toISOString()
-    });
-  }
+    }))
+  );
+
+  links.push(
+    tags.map((tag) => ({
+      url: concatPageUrl(`tags/${tag.slug}`),
+      lastmod: now,
+      changefreq: 'daily',
+      priority: 0.6
+    }))
+  );
 
   const stream = new SitemapStream({ hostname: constraints.sitemap.urlPrefix });
   const contentBuffer = await streamToPromise(Readable.from(links).pipe(stream));
