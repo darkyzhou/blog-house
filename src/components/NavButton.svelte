4ebe5b8541161c8cd1a47b5ea3942a2275bf1917
darkyzhou <darkyoooooo@gmail.com>
2021-03-09 22:52:03 +0800

    style(NavButton): add more styles

--- src/components/NavButton.svelte
+++ src/components/NavButton.svelte
@@ -9,6 +9,26 @@
     background-color: var(--bg-gray-900);
     opacity: 0.2;
   }
+
+  .caption {
+    background-color: var(--bg-gray-200);
+    opacity: 0;
+    transform: skew(100deg, -10deg) scale(0);
+    will-change: transform, opacity;
+    transition-property: transform, opacity;
+    transition-duration: 350ms;
+  }
+
+  .link:hover .caption,
+  .link:focus .caption {
+    opacity: 0.8;
+    transform: skew(180deg) scale(1);
+  }
+
+  .link:hover .imageMask,
+  .link:focus .imageMask {
+    filter: blur(2px);
+  }
 </style>
 
 <script>
@@ -18,12 +38,15 @@
   export let route;
 </script>
 
-<a sapper:prefetch class="block group {extraClasses} relative cursor-pointer" href="{route}">
-  <div class="imageMask relative">
-    <img alt="" src="{backgroundSrc}" class="object-cover pointer-events-none w-full h-full" />
-  </div>
-  <div
-    class="absolute inset-0 opacity-0 group-hover:opacity-100 h-full w-full grid place-items-center">
+<a
+  sapper:prefetch
+  class="link overflow-hidden grid cursor-pointer {extraClasses}"
+  style="grid-template-areas: 'stack'"
+  href="{route}">
+  <div class="caption z-10 h-full w-full grid place-items-center" style="grid-area: stack">
     <span class="text-2xl font-bold">{caption}</span>
+  </div>
+  <div class="imageMask relative" style="grid-area: stack">
+    <img alt="" src="{backgroundSrc}" class="object-cover pointer-events-none w-full h-full" />
   </div>
 </a>
