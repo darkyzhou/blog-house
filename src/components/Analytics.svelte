1023933b696662fdf8c924b608c22cd1cb0d9828
darkyzhou <darkyoooooo@gmail.com>
2021-03-20 13:23:38 +0800

    style: use carbon colors

---
+++ src/components/Analytics.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let gaMeasurementId;
+  export let baiduId;
+</script>
+
+{#if gaMeasurementId}
+  {@html `
+<script src="https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}" async></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', '${gaMeasurementId}');
+</script>`}
+{/if}
+{#if baiduId}
+  {@html `<script src="https://hm.baidu.com/hm.js?${baiduId}" async></script>`}
+{/if}
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,12 +2,26 @@
   .homeContainer {
     background: url('/background.jpg') fixed 50% / cover no-repeat;
   }
+
+  .dotsContainer {
+    background: var(--bg-carbongray-900);
+  }
+
+  .dots {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 16 16' style='enable-background:new 0 0 16 16' xml:space='preserve'%3E%3Crect width='16' fill='none' height='16'/%3E%3Crect x='0' y='0' fill='white' width='1' height='1'/%3E%3C/svg%3E");
+    background-size: 16px;
+    opacity: 0.25;
+  }
 </style>
 
 <script>
   import Nav from '../components/Nav.svelte';
   import Footer from '../components/Footer.svelte';
   import constraints from '../../config/constraints.json';
+  import Analytics from '../components/Analytics.svelte';
 
   export let segment;
 
@@ -16,24 +30,16 @@
 </script>
 
 <svelte:head>
-  {#if constraints.analytics.google.gaMeasurementId}
-    {@html `
-<script src="https://www.googletagmanager.com/gtag/js?id=${constraints.analytics.google.gaMeasurementId}" async></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-  gtag('config', '${constraints.analytics.google.gaMeasurementId}');
-</script>`}
-  {/if}
-  {#if constraints.analytics.baidu.id}
-    {@html `<script src="https://hm.baidu.com/hm.js?${constraints.analytics.baidu.id}" async></script>`}
-  {/if}
+  <Analytics
+    gaMeasurementId="{constraints.analytics.google.gaMeasurementId}"
+    baiduId="{constraints.analytics.baidu.id}" />
 </svelte:head>
 
-<div class="min-h-screen flex flex-col text-gray-200 {!segment ? 'homeContainer' : 'bg-gray-800'}">
+<div
+  class="min-h-screen flex flex-col text-gray-200 {!segment ? 'homeContainer' : 'dotsContainer'}">
+  {#if segment}
+    <span class="dots"></span>
+  {/if}
   <Nav segment="{segment}" />
   <main class="flex-grow flex flex-col items-center">
     <slot />
