9fcec46cfabaf0bfb90849ca7aaf22ef82fd55e4
darkyzhou <darkyoooooo@gmail.com>
2021-03-24 00:57:55 +0800

    fix(tags.json.js): fix the bug that causes a massive tags.json

--- src/routes/data/tags.json.js
+++ src/routes/data/tags.json.js
@@ -1,6 +1,16 @@
 import tags from '../../../shared/tags';
 
+const minifiedTags = tags.map((t) => ({
+  ...t,
+  articles: t.articles.map((a) => ({
+    ...a,
+    tableOfContent: undefined,
+    html: undefined,
+    pureTextContent: undefined
+  }))
+}));
+
 export async function get(req, res, next) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(tags));
+  res.end(JSON.stringify(minifiedTags));
 }
