704112eeeb2d314ac8d358d2e30bdc426a79a578
darkyzhou <darkyoooooo@gmail.com>
2021-03-24 11:33:52 +0800

    style(NavItem.svelte): fix missing rules for safari

--- src/components/NavItem.svelte
+++ src/components/NavItem.svelte
@@ -1,3 +1,18 @@
+<style>
+  @supports ((backdrop-filter: none) or (-webkit-backdrop-filter: none)) {
+    .inactive {
+      backdrop-filter: blur(24px);
+      -webkit-backdrop-filter: blur(24px);
+    }
+  }
+
+  @supports not ((backdrop-filter: none) or (-webkit-backdrop-filter: none)) {
+    .inactive {
+      background-color: rgba(38, 38, 38, 0.75);
+    }
+  }
+</style>
+
 <script>
   import { getIcon } from './icons';
 
@@ -14,7 +29,7 @@
   tabindex="0"
   class="grid place-items-center px-2 py-1 md:px-4 md:py-2 text-carbongray-100 outline-none border-transparent hover:border-carbongray-200 focus:border-carbongray-200 border-2 {active &&
     'text-carbongray-600 bg-carbongray-100'} {extraClasses}"
-  style="{!active ? 'backdrop-filter: blur(16px);' : ''}"
+  class:inactive="{!active}"
   href="{route}">
   {#if icon}
     <svelte:component this="{getIcon(icon)}" extraClasses="w-4 h-4" />
