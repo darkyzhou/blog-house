4f1cdbc20f5133716f1868b9e7e8eb4b8d68a581
darkyzhou <darkyoooooo@gmail.com>
2021-12-25 08:08:43 +0000

    feat(Footer.svelte): move additinal content to left

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -22,19 +22,7 @@
 
 <div class="w-full bg-carbongray-900">
   <footer
-    class="footer w-full max-w-[960px] mx-auto text-carbongray-300 text-sm py-2 sm:py-4 px-4 sm:px-12 flex justify-between items-end flex-wrap gap-2 {extraClasses}">
-    <div class="flex flex-col gap-1">
-      {#if footerConfiguration.leftHtml}
-        <p>
-          {@html footerConfiguration.leftHtml}
-        </p>
-      {/if}
-      {#if footerConfiguration.rightHtml}
-        <p>
-          {@html footerConfiguration.rightHtml}
-        </p>
-      {/if}
-    </div>
+    class="footer w-full max-w-[960px] mx-auto text-carbongray-300 text-sm py-2 sm:py-4 px-4 sm:px-12 flex justify-center sm:justify-between items-end flex-wrap gap-2 {extraClasses}">
     <div class="flex gap-6 text-carbongray-200 text-sm">
       {#if footerConfiguration.additional}
         {#each footerConfiguration.additional as item, i}
@@ -66,6 +54,18 @@
             </a>
           {/if}
         {/each}
+      {/if}
+    </div>
+    <div class="flex flex-col gap-1 text-center sm:text-right">
+      {#if footerConfiguration.leftHtml}
+        <p>
+          {@html footerConfiguration.leftHtml}
+        </p>
+      {/if}
+      {#if footerConfiguration.rightHtml}
+        <p>
+          {@html footerConfiguration.rightHtml}
+        </p>
       {/if}
     </div>
   </footer>
