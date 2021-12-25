9718990ffc366ce56adb6a5ce4070f9d241572c7
darkyzhou <darkyoooooo@gmail.com>
2021-12-25 08:52:04 +0000

    feat: block user agents for analytics

--- src/components/Analytics.svelte
+++ src/components/Analytics.svelte
@@ -9,9 +9,17 @@
   let enabled = false;
 
   onMount(() => {
-    enabled =
-      !!browser && !basicConfiguration.analytics.blockedDomains?.includes(window.location.hostname);
+    enabled = !!browser && !domainBlocked() && !uaBlocked();
   });
+
+  function domainBlocked() {
+    return basicConfiguration.analytics.blockedDomains?.includes(window.location.hostname);
+  }
+
+  function uaBlocked() {
+    const userAgent = navigator.userAgent.toLocaleLowerCase();
+    return basicConfiguration.analytics.blockedUAs?.some((ua) => userAgent.includes(ua));
+  }
 
   // workaround for a stupid vscode svelte plugin bug and a prettier svelte plugin bug,
   // both of them are still not fixed even after a whole fucking year
