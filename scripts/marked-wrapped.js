e6249f2378abbe6b00232d683982b15026c2c70d
darkyzhou <darkyoooooo@gmail.com>
2021-12-27 16:07:34 +0000

    feat: add custom messages for markdown

--- scripts/marked-wrapped.js
+++ scripts/marked-wrapped.js
@@ -20,14 +20,61 @@ renderer.link = (href, title, text) => {
 
 const originalCodeRenderer = renderer.code;
 renderer.code = (code, info, escaped) => {
-  const result = originalCodeRenderer.call(renderer, code, info, escaped);
+  const result = originalCodeRenderer.call(renderer, code, info, escaped).trim();
+  if (/<pre><code[^>]*><div class/i.test(result)) {
+    return result.substring(result.indexOf('<div'), result.lastIndexOf('</div>') + 6);
+  }
   return `${result
-    .replace(/^<pre><code[^>]*>/is, '')
+    .replace(/^<pre><code[^>]*>/i, '')
     .trim()
     .replace(/<\/pre>(.*)<\/pre>$/is, '')}<div class='copy'></div></pre>\n`;
 };
 
+const messages = {
+  info: {
+    caption: '提示',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--carbon" width="16" height="16" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><defs></defs><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 6a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 8zm4 16.125h-8v-2.25h2.875v-5.75H13v-2.25h4.125v8H20z" fill="currentColor"></path></svg>'
+  },
+  warn: {
+    caption: '警告',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--carbon" width="16" height="16" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path d="M16 2C8.3 2 2 8.3 2 16s6.3 14 14 14s14-6.3 14-14S23.7 2 16 2zm-1.1 6h2.2v11h-2.2V8zM16 25c-.8 0-1.5-.7-1.5-1.5S15.2 22 16 22s1.5.7 1.5 1.5S16.8 25 16 25z" fill="currentColor"></path></svg>'
+  },
+  check: {
+    caption: '提示',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--carbon" width="16" height="16" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><defs></defs><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586z" fill="currentColor"></path></svg>'
+  },
+  exception: {
+    caption: '警告',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--carbon" width="16" height="16" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path d="M16 2C8.2 2 2 8.2 2 16s6.2 14 14 14s14-6.2 14-14S23.8 2 16 2zm5.4 21L16 17.6L10.6 23L9 21.4l5.4-5.4L9 10.6L10.6 9l5.4 5.4L21.4 9l1.6 1.6l-5.4 5.4l5.4 5.4l-1.6 1.6z" fill="currentColor"></path></svg>'
+  }
+};
+const validTypes = Object.getOwnPropertyNames(messages);
+
+function resolveMessageOptions(input) {
+  const regex = /^\$([+-]?)(\w+)(?:"(.*)")?$/i;
+  const result = regex.exec(input);
+  return {
+    showIcon: !result[1] || result[1] === '+',
+    type: validTypes.includes(result[2]) ? result[2] : 'info',
+    caption: result[3] || null
+  };
+}
+
+function toHtml(option, content) {
+  const { showIcon, type, caption } = resolveMessageOptions(option);
+  return `<div class="bhem bh-${type}">
+  ${!(showIcon || caption) ? '' : '<div class="bhemh">'}
+  ${!showIcon ? '' : `<span class="bhemi">${messages[type].svg}</span>`}
+  ${!caption ? '' : `<span class="bhemc">${caption || messages[type].caption}</span>`}
+  ${!(showIcon || caption) ? '' : '</div>'}
+  ${marked(content)}
+  </div>`;
+}
+
 const highlight = (code, lang) => {
+  if (lang?.startsWith('$')) {
+    return toHtml(lang, code);
+  }
   if (!highlighter.getLoadedLanguages().includes(lang)) {
     return highlighter.codeToHtml(code, {});
   }
---
+++ source/_posts/custom.md
@@ -0,0 +1,48 @@
+---
+title: Blog House 拓展 Markdown 测试
+date: 2021-12-27
+category: 测试
+tags:
+  - 测试测试测试
+---
+
+下面是一个信息：
+
+```$info
+I am a info!
+
+### some heading here
+
+some [link](https://github.com/darkyzhou) here
+
+```
+
+```$-info
+I am a info!
+```
+
+```$info"我是标题"
+I am a info!
+```
+
+```$-info"我是标题"
+I am a info!
+```
+
+下面是一个警告：
+
+```$warn
+I am a warning!
+```
+
+下面是一个勾：
+
+```$check
+I am a check!
+```
+
+下面是一个错误：
+
+```$exception
+I am an exception!
+```
