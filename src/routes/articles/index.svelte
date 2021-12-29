deb949d7bbe43b5a3cfcfdcdbc389ed4778a0abc
darkyzhou <darkyoooooo@gmail.com>
2021-12-29 06:30:58 +0000

    feat: add BackToTop

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -9,13 +9,34 @@
 <script>
   import { makeTitle, WaterflowController } from '../_utils';
   import ArticleCard from '../../components/ArticleCard.svelte';
+  import BackToTop from '../../components/BackToTop.svelte';
+  import debounce from 'debounce';
 
   export let articles;
+
   let realArticles = articles.filter((p) => !p.isPageArticle);
   let columns = [];
   let controller = new WaterflowController('articles-page', 336, 3, realArticles, (c) => {
     columns = c;
   });
+
+  let containerElement;
+  let showBackToTop = false;
+  let scrollToTopListener;
+
+  function checkShouldScrollToTop() {
+    showBackToTop = containerElement?.getBoundingClientRect().top < 0;
+  }
+
+  function scrollEvent() {
+    scrollToTopListener = debounce(() => checkShouldScrollToTop(), 100);
+    document.addEventListener('scroll', scrollToTopListener, false);
+    return {
+      destroy: () => {
+        document.removeEventListener('scroll', scrollToTopListener);
+      }
+    };
+  }
 </script>
 
 <svelte:head>
@@ -24,6 +45,8 @@
 
 <div
   class="my-6 sm:my-8 px-4 sm:px-8 flex gap-6 w-full max-w-300 justify-center"
+  bind:this={containerElement}
+  use:scrollEvent
   use:controller.observeResize
 >
   {#each columns as column}
@@ -34,6 +57,8 @@
     </div>
   {/each}
 </div>
+
+<BackToTop show={showBackToTop} />
 
 <!-- workaround a bug that sveltekit static adapter cannot detect sveltekit:prefetch inside deeply nested <ArticleCard> structures here -->
 <div class="hidden">
--- src/routes/tags/[tag].svelte
+++ src/routes/tags/[tag].svelte
@@ -11,7 +11,9 @@
 <script>
   import ArticleCard from '../../components/ArticleCard.svelte';
   import TagCard from '../../components/TagCard.svelte';
+  import BackToTop from '../../components/BackToTop.svelte';
   import { makeTitle, WaterflowController } from '../_utils';
+  import debounce from 'debounce';
 
   export let tag;
 
@@ -19,18 +21,36 @@
   let controller = new WaterflowController('tag-page', 336, 3, tag.articles, (c) => {
     columns = c;
   });
+
+  let cardElement;
+  let showBackToTop = false;
+  let scrollToTopListener;
+
+  function checkShouldScrollToTop() {
+    showBackToTop = cardElement?.getBoundingClientRect().top < 0;
+  }
+
+  function scrollEvent() {
+    scrollToTopListener = debounce(() => checkShouldScrollToTop(), 100);
+    document.addEventListener('scroll', scrollToTopListener, false);
+    return {
+      destroy: () => {
+        document.removeEventListener('scroll', scrollToTopListener);
+      }
+    };
+  }
 </script>
 
 <svelte:head>
   <title>{makeTitle(`标签：${tag.name}`)}</title>
   {#if tag.description}
-    <meta name="description" content="{tag.description}" />
+    <meta name="description" content={tag.description} />
   {/if}
 </svelte:head>
 
-<div class="my-4 sm:my-8 px-8 w-full" use:controller.observeResize>
-  <div class="my-8 mx-auto max-w-64">
-    <TagCard item="{tag}" displayMode="{true}" />
+<div class="my-4 sm:my-8 px-8 w-full" use:controller.observeResize use:scrollEvent>
+  <div class="my-8 mx-auto max-w-64" bind:this={cardElement}>
+    <TagCard item={tag} displayMode={true} />
   </div>
   <div class="flex gap-6 w-full max-w-300 justify-center">
     {#if tag.articles?.length <= 0}
@@ -39,10 +59,12 @@
       {#each columns as column}
         <div class="flex-1 flex flex-col gap-4 max-w-[336px]">
           {#each column as article}
-            <ArticleCard article="{article}" />
+            <ArticleCard {article} />
           {/each}
         </div>
       {/each}
     {/if}
   </div>
 </div>
+
+<BackToTop show={showBackToTop} />
