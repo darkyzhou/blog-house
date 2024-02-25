a667460c9f3bf8caa94e02caaaa2dd52bbc76b22
darkyzhou <darkyoooooo@gmail.com>
2024-02-25 21:51:28 +0800

    fix: fix bugs

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,4 @@
 export const trailingSlash = 'always';
 export const prerender = true;
 export const ssr = true;
+export const csr = true;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,3 @@
-import { sample } from 'lodash-es';
-
 export const OVERLAY_SCROLLBAR_SETTINGS_BODY = {
   scrollbars: { theme: 'os-theme-body' }
 };
@@ -11,11 +9,3 @@ export const OVERLAY_SCROLLBAR_SETTINGS_OTHER = {
     autoHideDelay: 200
   }
 };
-
-export const BACKGROUND_OFFSETS = {
-  background_1: ['center', '35%'],
-  background_2: ['center', '45%'],
-  background_3: ['center', '40%']
-};
-
-export const BACKGROUND_NAME = sample(Object.keys(BACKGROUND_OFFSETS));
